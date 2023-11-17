6d2c8c0700935a53a41281e8d1500eccafd918fd
Tim <timothy8@gmail.com>
2023-11-17 04:13:55 +0300

    Initial commit

--- app/src/main/java/com/example/propertypro/SplashScreenActivity.kt
+++ app/src/main/java/com/example/propertypro/SplashScreenActivity.kt
@@ -2,12 +2,8 @@ package com.example.propertypro
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.example.propertypro.databinding.ActivityLoginBinding
-import com.example.propertypro.databinding.ActivitySignupBinding
-import com.example.propertypro.databinding.SplashScreenBinding
 import com.firebase.ui.auth.AuthMethodPickerLayout
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
@@ -15,71 +11,76 @@ import com.google.firebase.auth.FirebaseAuth
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Completable
 import java.util.Arrays
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseUser
 import java.util.concurrent.TimeUnit
 
 class SplashScreenActivity : AppCompatActivity() {
 
     private lateinit var providers: List<AuthUI.IdpConfig>
     private lateinit var firebaseAuth: FirebaseAuth
-    private lateinit var listener:FirebaseAuth.AuthStateListener
+    private lateinit var listener: FirebaseAuth.AuthStateListener
 
-    private lateinit var binding: SplashScreenBinding
-
-    companion object{
+    companion object {
         private val LOGIN_REQUEST_CODE = 7171
-
     }
-
 
     override fun onStart() {
         super.onStart()
-        delayMainActivity();
+        delayMainActivity()
     }
 
     override fun onStop() {
-        if(firebaseAuth !== null && listener != null) firebaseAuth.removeAuthStateListener(listener)
+        if (firebaseAuth !== null && listener != null) firebaseAuth.removeAuthStateListener(listener)
         super.onStop()
     }
-    private fun delayMainActivity(){
 
+    private fun delayMainActivity() {
         Completable.timer(2000, TimeUnit.MILLISECONDS, AndroidSchedulers.mainThread())
             .subscribe {
                 showLoginLayout()
             }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.layout_sign_in)
         init()
     }
 
-    private fun init(){
+    private fun init() {
         providers = Arrays.asList(
             AuthUI.IdpConfig.PhoneBuilder().build(),
-            AuthUI.IdpConfig.GoogleBuilder().build()
+            AuthUI.IdpConfig.EmailBuilder().build()
         )
 
         firebaseAuth = FirebaseAuth.getInstance()
-        listener = FirebaseAuth.AuthStateListener { myFirebaseAuth->
+        listener = FirebaseAuth.AuthStateListener { myFirebaseAuth ->
             val user = myFirebaseAuth.currentUser
-            if (user != null)
-                Toast.makeText(this@SplashScreenActivity,"Welcome:"+user.uid,Toast.LENGTH_SHORT).show()
-            else
+            if (user != null) {
+                if (!user.isEmailVerified) {
+                    // User has not verified their email
+                    Toast.makeText(
+                        this@SplashScreenActivity,
+                        "Please verify your email before signing in.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(this@SplashScreenActivity, "Welcome: ${user.uid}", Toast.LENGTH_SHORT).show()
+                }
+            } else {
                 showLoginLayout()
+            }
         }
 
-
+        firebaseAuth.addAuthStateListener(listener)
     }
 
-    private fun showLoginLayout(){
-
+    private fun showLoginLayout() {
         val authMethodPickerLayout = AuthMethodPickerLayout.Builder(R.layout.layout_sign_in)
             .setPhoneButtonId(R.id.btn_phone_sign_in)
-            .setGoogleButtonId(R.id.btn_google_sign_in)
+            .setEmailButtonId(R.id.btn_email_sign_in)
             .build()
-
-
-
 
         val signInIntent = AuthUI.getInstance()
             .createSignInIntentBuilder()
@@ -90,14 +91,7 @@ class SplashScreenActivity : AppCompatActivity() {
             .build()
 
         startActivityForResult(signInIntent, LOGIN_REQUEST_CODE)
-
-
-
-
     }
-
-
-
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -105,13 +99,35 @@ class SplashScreenActivity : AppCompatActivity() {
             val response = IdpResponse.fromResultIntent(data)
             if (resultCode == Activity.RESULT_OK) {
                 val user = FirebaseAuth.getInstance().currentUser
+                if (user != null) {
+                    val userEmail = user.email
+                    // Now you have the email address entered by the user
+                    if (!user.isEmailVerified) {
+                        sendEmailVerification(user)
+                    }
+                }
             } else {
                 Toast.makeText(this@SplashScreenActivity, "" + response?.error?.message, Toast.LENGTH_SHORT).show()
             }
         }
-
     }
 
-
-
+    private fun sendEmailVerification(user: FirebaseUser) {
+        user.sendEmailVerification()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                        this@SplashScreenActivity,
+                        "Verification email sent to ${user.email}. Please verify before signing in.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        this@SplashScreenActivity,
+                        "Failed to send verification email. ${task.exception?.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
 }
