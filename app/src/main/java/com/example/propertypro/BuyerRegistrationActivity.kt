89a276d076460aa4cd84ea03ced1b435c1473898
Tim <timothy8@gmail.com>
2023-11-18 20:41:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/propertypro/BuyerRegistrationActivity.kt
@@ -0,0 +1,4 @@
+package com.example.propertypro
+
+class BuyerRegistrationActivity {
+}
---
+++ app/src/main/java/com/example/propertypro/HomeScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.example.propertypro
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen) // Replace with your actual layout file
+        // Add any additional logic or UI setup specific to the home screen
+    }
+}
