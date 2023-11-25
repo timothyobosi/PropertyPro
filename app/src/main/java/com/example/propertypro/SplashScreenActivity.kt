ae0ee957e127a19c38b51acc46a64986f09d01c3
Tim <timothy8@gmail.com>
2023-11-25 08:48:17 +0300

    Update

--- app/src/main/java/com/example/propertypro/SplashScreenActivity.kt
+++ app/src/main/java/com/example/propertypro/SplashScreenActivity.kt
@@ -1,155 +1,43 @@
 package com.example.propertypro
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
-import com.firebase.ui.auth.AuthMethodPickerLayout
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.IdpResponse
-import com.google.firebase.auth.FirebaseAuth
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.core.Completable
-import java.util.Arrays
+import android.os.Handler
+import android.os.Looper
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseUser
-import java.util.concurrent.TimeUnit
-import android.view.View
-
-
+import com.example.propertypro.databinding.ActivitySplashScreenBinding
 
 class SplashScreenActivity : AppCompatActivity() {
 
-    private lateinit var providers: List<AuthUI.IdpConfig>
-    private lateinit var firebaseAuth: FirebaseAuth
-    private lateinit var listener: FirebaseAuth.AuthStateListener
-
-    companion object {
-        private val LOGIN_REQUEST_CODE = 7171
-    }
-
-    override fun onStart() {
-        super.onStart()
-        delayMainActivity()
-    }
-
-    override fun onStop() {
-        if (firebaseAuth !== null && listener != null) firebaseAuth.removeAuthStateListener(listener)
-        super.onStop()
-    }
-
-    private fun delayMainActivity() {
-        Completable.timer(2000, TimeUnit.MILLISECONDS, AndroidSchedulers.mainThread())
-            .subscribe {
-                showLoginLayout()
-            }
-    }
+    private lateinit var binding: ActivitySplashScreenBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout_sign_in)
-        init()
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Assuming you want to show the ProgressBar for a few seconds and then hide it
+        showProgressBar()
+
+        // Simulate a delay (e.g., network request, data loading)
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Hide the ProgressBar after a delay
+            hideProgressBar()
+
+            // Launch LoginActivity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }, 3000) // Adjust the delay time as needed
     }
 
-    private fun init() {
-        providers = Arrays.asList(
-            AuthUI.IdpConfig.PhoneBuilder().build(),
-            AuthUI.IdpConfig.EmailBuilder().build()
-        )
-
-        firebaseAuth = FirebaseAuth.getInstance()
-        listener = FirebaseAuth.AuthStateListener { myFirebaseAuth ->
-            val user = myFirebaseAuth.currentUser
-            if (user != null) {
-                if (!user.isEmailVerified) {
-                    // User has not verified their email
-                    Toast.makeText(
-                        this@SplashScreenActivity,
-                        "Welcome, Thank you for trusting us.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                } else {
-                    Toast.makeText(this@SplashScreenActivity, "Welcome: ${user.uid}", Toast.LENGTH_SHORT).show()
-                }
-            } else {
-                showLoginLayout()
-            }
-        }
-
-        firebaseAuth.addAuthStateListener(listener)
+    private fun showProgressBar() {
+        binding.progressBar.visibility = ProgressBar.VISIBLE
     }
 
-    private fun showLoginLayout() {
-        val authMethodPickerLayout = AuthMethodPickerLayout.Builder(R.layout.layout_sign_in)
-
-            .setPhoneButtonId(R.id.btn_phone_sign_in)
-            .setEmailButtonId(R.id.btn_email_sign_in)
-            .build()
-
-        val signInIntent = AuthUI.getInstance()
-            .createSignInIntentBuilder()
-            .setAuthMethodPickerLayout(authMethodPickerLayout)
-            .setTheme(R.style.LoginTheme)
-            .setAvailableProviders(providers)
-            .setIsSmartLockEnabled(false)
-            .build()
-
-        startActivityForResult(signInIntent, LOGIN_REQUEST_CODE)
+    private fun hideProgressBar() {
+        binding.progressBar.visibility = ProgressBar.GONE
     }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == LOGIN_REQUEST_CODE) {
-            val response = IdpResponse.fromResultIntent(data)
-            if (resultCode == Activity.RESULT_OK) {
-                val user = FirebaseAuth.getInstance().currentUser
-                if (user != null) {
-                    if (!user.isEmailVerified) {
-                        sendEmailVerification(user)
-                    }
-                    // Redirect to the layout where the user inputs the verification code
-                    // You can call a new function to handle this, e.g., showVerificationCodeLayout()
-                    showVerificationCodeLayout(user)
-                }
-            } else {
-                Toast.makeText(this@SplashScreenActivity, "" + response?.error?.message, Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
-    private fun showVerificationCodeLayout(user: FirebaseUser) {
-        // Start a new activity for entering verification code
-        val verificationIntent = Intent(this@SplashScreenActivity, VerificationCodeActivity::class.java)
-        // Pass user email to the VerificationCodeActivity
-        verificationIntent.putExtra("EMAIL", user.email)
-        startActivity(verificationIntent)
-        finish() // Optional: Close the login activity
-    }
-
-    fun redirectToCategorySelection(view: View) {
-        val intent = Intent(this@SplashScreenActivity, CategorySelectionActivity::class.java)
-        startActivity(intent)
-        finish()
-    }
-
-    private fun sendEmailVerification(user: FirebaseUser) {
-        user.sendEmailVerification()
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    Toast.makeText(
-                        this@SplashScreenActivity,
-                        "Verification email sent to ${user.email}. Please verify before signing in.",
-                        Toast.LENGTH_LONG
-                    ).show()
-                } else {
-                    Toast.makeText(
-                        this@SplashScreenActivity,
-                        "Failed to send verification email. ${task.exception?.message}",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-    }
-
-
 }
